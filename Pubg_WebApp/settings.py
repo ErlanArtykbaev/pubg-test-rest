12a9042e106fe0a9cd4f03992cfded16f2c2f4d4
erlan artykbaev <erik.2405@mail.ru>
2020-11-02 14:40:21 +0600

    static files

--- Pubg_WebApp/settings.py
+++ Pubg_WebApp/settings.py
@@ -174,6 +174,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
